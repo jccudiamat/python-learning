c108515a0ddc5a42d988cb6b19acad9c3d46552b
Jessie <jccudiamat55@gmail.com>
2024-12-31 02:37:03 +0400

    Python Terms

---
+++ basics/1.1_python_terms.py
@@ -0,0 +1,18 @@
+##Object: A data record with fields; and instance of a class
+#Class --> str
+#Objects --> "your_name"
+#description
+#help(str)
+
+##Variables: The reference to Objects
+#Variable: A defined name that references an object
+job_location = "United Arab Emirates"
+#variable_name --> object
+#id(job_location)
+
+#Assigning a function to a variable
+print_func = print
+print_func("Hello,World!")
+
+
+
