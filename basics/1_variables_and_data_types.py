30c07cc2f983a2a405927bab84f9007360ae9890
Jessie <jccudiamat55@gmail.com>
2024-12-31 01:51:22 +0400

    Variables and Data types

---
+++ basics/1_variables_and_data_types.py
@@ -0,0 +1,32 @@
+#variable_name = value
+#Data Types
+
+##int
+salary = 100000
+print(salary)
+
+##float
+base_salary = 100000
+bonus_rate = 0.10
+total_salary = (base_salary) * (1 + bonus_rate)
+print(total_salary)
+
+##str
+company_name = 'Ratel Labs'
+print(company_name)
+
+##bool
+job_wfh = True
+print(not job_wfh)
+
+job_id = 101
+job_title = 'Data Analyst'
+job_salary = '125000'
+
+print("Data Job Description")
+print("********************")
+print("Job ID:      ",job_id)
+print("Company name:",company_name)
+print("Job Title:   ",job_title)
+print("Salary:      ",job_salary)
+print("WFH:         ",job_wfh)
