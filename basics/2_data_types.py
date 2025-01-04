a1f08830e598bf97b7a985169b68b076c5f8db92
Jessie <jccudiamat55@gmail.com>
2025-01-04 20:14:04 +0400

    Data types

--- basics/2_data_types.py
+++ basics/2_data_types.py
@@ -1,1 +1,61 @@
-"#Data types" 
+#Data types
+#Use help() function to learn methods 
+
+total_salary = 110000
+bonus_salary = 10000
+##Check data type
+print(type(total_salary))
+
+base_salary = total_salary - bonus_salary
+print(base_salary)
+
+##TEXT
+#str --> String
+job_title = 'Data Analyst'
+remove_word = 'Data'
+#help(str)
+
+fname = "data"
+lname = "scientist"
+fullname = fname + lname
+print(fullname.capitalize())
+
+##NUMERIC
+#int --> 42
+job_id = int(3.14)
+print(job_id)
+print(type(job_id))
+
+#float --> 3.14
+
+#complex --> 1 + 2j
+
+##SEQUENCE
+#list --> [1,2,3]
+
+#tuple --> {1,2,3}
+
+#range --> range(10)
+
+##MAPPING
+#dict --> {"key": "value"}
+
+##SET
+#set --> {1,2,3}
+
+#frozenset --> frozenset({1,2,3})
+
+##BOOLEAN
+#bool --> True or False
+
+##BINARY
+#bytes --> b"Data"
+
+#bytearray --> bytearray(5)
+
+#memoryview --> memory(b"Data")
+
+
+
+
+
