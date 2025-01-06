5489819de477a91b834f4309969fa01ecb24e59e
Jessie <jccudiamat55@gmail.com>
2025-01-06 22:33:13 +0400

    Check sign

---
+++ practice-problems/positive_or_negative.py
@@ -0,0 +1,9 @@
+while True:
+    sample_num = int(input("Enter number: "))
+    if sample_num == 0:
+        print("Cannot be zero")
+    elif sample_num > 0:
+        print(sample_num," is positive")
+    else:
+        print(sample_num," is negative")
+    
