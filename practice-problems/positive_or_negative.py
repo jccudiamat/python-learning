9358762d49e4f865c5effcc7d6273c0c923edc74
Jessie <jccudiamat55@gmail.com>
2025-01-06 22:39:39 +0400

    Refactor Code

--- practice-problems/positive_or_negative.py
+++ practice-problems/positive_or_negative.py
@@ -1,9 +1,14 @@
 while True:
-    sample_num = int(input("Enter number: "))
-    if sample_num == 0:
-        print("Cannot be zero")
-    elif sample_num > 0:
-        print(sample_num," is positive")
-    else:
-        print(sample_num," is negative")
+    try:
+        sample_num = int(input("Enter number: "))
+        if sample_num == 0:
+            print("Cannot be zero")
+        elif sample_num > 0:
+            print(sample_num," is positive")
+        else:
+            print(sample_num," is negative")
+    except ValueError:
+        print("Type numbers only")
+    print()
+
     
