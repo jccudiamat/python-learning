2afe95124fad84ca82f59cf71e644f3ec7d7def0
Jessie <jccudiamat55@gmail.com>
2025-01-09 14:16:20 +0400

    Check string in list

---
+++ practice-problems/value_in_string.py
@@ -0,0 +1,24 @@
+#value in string
+#Your task is to write a program that:
+#1)checks if a string is inside a list of strings
+#2)outputs a confirmation message if it is
+
+names = ["Mark Rober","Backyard Scientist","Vsauce"]
+
+youtuber = "Mark Rober"
+
+#using for loop
+for name in names:
+    if youtuber==name:
+         print(f"{name} exists in the list {names} in index {names.index(name)}")
+         break
+    else:
+         print(f"{youtuber} does not exist!")
+         
+#using if statement
+if youtuber in names:
+    print(f"{youtuber} exists in the list {names} in index {names.index(youtuber)}")
+else:
+    print(f"{youtuber} does not exist!")
+   
+    
