8a22714571e84d96429fc57081bbe44d06158ea2
Jessie <jccudiamat55@gmail.com>
2025-01-10 00:27:12 +0400

    Plateletcrit

--- practice-problems/plateletcrit.py
+++ practice-problems/plateletcrit.py
@@ -1,7 +1,7 @@
 #Normal range for platelets is typically between 150,000 to 450,000 per microlite
+#The normal range for PCT is 0.22% to 0.36%.
 #Plateletcrit (PCT): This is a measure of the volume occupied by platelets in the blood as a percentage.
 #It is calculated using the formula:
-#The normal range for PCT is 0.22% to 0.24%.
 #PCT=platelet count×mean platelet volume (MPV)/10,000
 while True:
     try:
